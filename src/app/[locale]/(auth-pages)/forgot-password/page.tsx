9345d844fdccaa3edc0ad2a190e43131775931d2
Kinetic639 <michal.stepien36@gmail.com>
2025-05-16 08:19:39 +0200

    dashboards layouts

--- src/app/[locale]/(auth-pages)/forgot-password/page.tsx
+++ src/app/[locale]/(auth-pages)/forgot-password/page.tsx
@@ -1,12 +1,15 @@
 import { ForgotPasswordForm } from "@/components/auth/forms/forgot-password-form";
 import { Message } from "@/components/form-message";
 import { SmtpMessage } from "../smtp-message";
+import { AuthCard } from "@/components/auth/AuthCard";
 
 export default async function ForgotPassword(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
   return (
     <>
-      <ForgotPasswordForm message={searchParams} />
+      <AuthCard>
+        <ForgotPasswordForm message={searchParams} />
+      </AuthCard>
       <SmtpMessage />
     </>
   );
