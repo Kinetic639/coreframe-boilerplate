6a76358d70f538a1504a02a7970e3c8c39020c96
Kinetic639 <michal.stepien36@gmail.com>
2025-10-22 10:10:10 +0000

    status layout change

--- src/components/Dashboard/DashboardStatusBar.tsx
+++ src/components/Dashboard/DashboardStatusBar.tsx
@@ -3,7 +3,7 @@ import { StatusBarMessage } from "./StatusBarMessage";
 
 export function DashboardStatusBar() {
   return (
-    <div className="fixed inset-x-0 bottom-0 z-50 flex h-8 items-center justify-between border-t bg-background px-4 text-xs shadow-lg">
+    <div className="sticky bottom-0 z-20 flex h-8 items-center justify-between border-t bg-background px-4 text-xs shadow-lg">
       <div className="flex items-center space-x-2">
         <CompactBranchSelector />
       </div>
